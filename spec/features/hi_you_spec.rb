1ac5035617978ca7e579799a1827dbbaf7cfa7d2
Alice Lieutier <alice.lieutier@gmail.com>
2020-10-21 13:21:04 +0100

    Nicer test infrastructure

--- spec/features/hi_you_spec.rb
+++ spec/features/hi_you_spec.rb
@@ -1,9 +1,4 @@
-require "capybara/rspec"
-require_relative "../../lib/app"
-
-Capybara.app = SampleApp
-
-feature "home page says hi" do
+feature "Home page says hi" do
   scenario "loads the home page and sees Hi you!" do
     visit("/")
     expect(page).to have_content "Hi you!"
